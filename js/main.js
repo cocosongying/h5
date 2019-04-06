9db9b329f3cec40ee88501a1fb7a68d0eaf177cc
cocosongying <cocosongying@gmail.com>
2019-04-06 22:42:32 +0800

    init 1.0

---
+++ js/main.js
@@ -0,0 +1,31 @@
+(function () {
+    const navBtn=document.querySelector(".header-navbar-btn");
+    navBtn.addEventListener("click", function () {
+        this.classList.toggle("active");
+    })
+})();
+var article_list=[
+    {"title":"My Heart Is With You","desc":"满屏心心","img":"","large":"","datetime":"2019-02-14","link":"/my_heart_is_with_you.html","coverlink":""}
+];
+var innerHtml="";
+for (let article of article_list) {
+    var card='<div class="card-item">';
+    var cover='';
+    if (article.large) {
+        card='<div class="card-item large">';
+    }
+    if (article.img) {
+        card='<div class="card-item img">';
+        if(article.coverlink){
+            cover='<div class="card-cover" style="background-image: url('+article.coverlink+')"></div>';
+        }
+        if(article.large){
+            card='<div class="card-item large img">';
+        }
+    }
+    innerHtml += card+'<article>'+cover+'<a class="article-link" target="_blank" href="'+article.link+'" itemprop="url"></a>';
+    innerHtml += '<h2 class="article-title">'+article.title+'</h2>';
+    innerHtml += '<div class="article-meta"><span>'+article.desc+'</span><br><strong>cocosongying</strong> <span>发布于</span> <a href="'+article.link+'" class="article-date">';
+    innerHtml += '<time datetime="'+article.datetime+'" itemprop="datePublished">'+article.datetime+'</time></a></div></article></div>'
+}
+document.getElementById("card_list").innerHTML=innerHtml;
